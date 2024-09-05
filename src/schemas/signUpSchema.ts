98f91200c01eb19d29e3813a9104bbfc4e29c85a
Shihan <sharifulislamshihan@gmail.com>
2024-09-05 20:39:43 +0600

    zod installed, singup and verify code schema validation by zod implimented

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const userNameValidation = z
+    .string()
+    .min(2, "Username must be atleast 2 characters")
+    .max(20, "Username must be not more than 20 characters")
+    .regex(/^[a-zA-Z0-9]{2,20}$/, "Username must not contain special character")
+
+
+export const signUpSchema = z.object({
+    userName: userNameValidation,
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be atleast 6 characters"),
+})
